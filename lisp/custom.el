ed6c1c887fe6461cbe5dee7c2cbb6d4fad6aad78
3dimaging <weizheli@gmail.com>
2018-06-07 08:58:57 -0400

    Add files via upload

---
+++ lisp/custom.el
@@ -0,0 +1,22 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("05fd42bef42991c45ecb17ecad1ff38c10e8faac51efea44f0782bdaebaf4211" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "3629b62a41f2e5f84006ff14a2247e679745896b5eaa1d5bcfbc904a3441b0cd" default)))
+ '(evil-leader/leader "SPC")
+ '(evil-want-C-u-scroll t)
+ '(package-selected-packages
+   (quote
+    (company-jedi projectile projectile-codesearch projectile-git-autofetch counsel-projectile avy lispy rainbow-delimiters dired-details+ dired-rainbow dired-sidebar dired-sort dired-sort-menu dired-sort-menu+ diredful latex-extra latex-math-preview latex-pretty-symbols latex-unicode-math-mode ess ess-R-data-view ess-smart-equals ess-smart-underscore ess-view markdown-mode markdown-mode+ color-theme ahungry-theme dired+ tabbar-ruler treemacs treemacs-evil treemacs-projectile auctex use-package 2048-game auto-yasnippet cdlatex counsel ein elpy evil-leader evil-nerd-commenter evil-surround exec-path-from-shell expand-region find-file-in-project flycheck ggtags helm-ag helm-gtags highlight-indentation hungry-delete iedit js2-refactor latex-preview-pane lua-mode magit magit-popup monokai-theme multiple-cursors mwe-log-commands nodejs-repl org-pomodoro pallet pkg-info popup popwin powerline-evil py-autopep8 pyvenv request-deferred reveal-in-osx-finder s shut-up skewer-mode smartparens smartrep solarized-theme swiper undo-tree web-mode websocket which-key window-numbering with-editor yasnippet auctex-latexmk auctex-lua auto-complete-auctex company-auctex company-ebdb company-math company-statistics company))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(iedit-occurrence ((t (:inherit region)))))
+
+;;(provide 'custom)
